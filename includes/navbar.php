7cc182b7ed2f5d8b5a8b0b890ecf17750d0ef2e4
Candice <candice.doise@gmail.com>
2022-11-29 16:07:39 +0100

    navbar

---
+++ includes/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar">
+    <div class="logo">
+    <a href="index.php"><img class="logo-img" src="images\Les Voix de l’Audomarois.svg" alt="logo voix de l'audomarois"></a>
+    </div>
+    <div class="link">
+    <ul>
+        <li>
+            <a href="">Admin</a>
+        </li>
+        <li>
+            <a href="">Mon Suivi</a>
+        </li>
+        <li>
+            <a href="">Contact</a>
+        </li>
+        <li>
+            <a href=""><i class="color-link text-dark fa-solid fa-power-off"></i></a>
+        </li>
+    </ul>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$title = "Accueil";
+require('includes/header.php');
+require('includes/navbar.php');
+
+
+
+require('includes/footer.php');
+?>
